dd21a13c3684a91e665887c3c428b2c6070d5d36
David Merrick <david@david-merrick.com>
2018-11-09 16:16:00 -0800

    Added a filter to the labels

--- index.js
+++ index.js
@@ -8,26 +8,36 @@ const CONFIG_FILE = "config.json";
 
 fs.readFile(CONFIG_FILE, 'utf8', (err, data) => {
 	let config = JSON.parse(data);
-	let stops = config.stops;
+    let stops = config.stops;
+    let query = process.argv[2] || "";
 
-	let promises = stops.map(stop => 
-        fetch("https://developer.trimet.org/ws/V1/arrivals?json=true&locIDs=" + stop.stopId + "&appID=" + config.trimetApiKey)
-            .then(response => response.json())
-            .then(json => {
-                let arrivals = json.resultSet.arrival;
-                arrivals.sort((a, b) => getNextArrivalTime(a) - getNextArrivalTime(b));
-                return arrivals.filter(a => a.route == stop.busId)
-                               .map(a => formatArrival(a, stop));
-                
-            })
-    );
+    let promises = stops
+        .filter(stop => stop.label.indexOf(query) != -1)
+        .map(stop => 
+            fetch("https://developer.trimet.org/ws/V1/arrivals?json=true&locIDs=" + stop.stopId + "&appID=" + config.trimetApiKey)
+                .then(response => response.json())
+                .then(json => {
+                    let arrivals = json.resultSet.arrival;
+                    arrivals.sort((a, b) => getNextArrivalTime(a) - getNextArrivalTime(b));
+                    return arrivals.filter(a => a.route == stop.busId)
+                                .map(a => formatArrival(a, stop));
+                    
+                })
+        );
 
 	Promise.all(promises).then(results => {
-		// flatMap output
-        let output = results.reduce((a, b) => a.concat(b)); 
-        alfy.output(output);
+        alfy.output(getOutput(results));
 	});
 });
+
+function getOutput(results){
+    if(results.length == 0){
+        return [];
+    }
+
+    // flatMap output
+    return results.reduce((a, b) => a.concat(b));
+}
 
 function formatArrival(arrival, stop) {
     let minsFromNow = getMinutesFromNow(getNextArrivalTime(arrival));
